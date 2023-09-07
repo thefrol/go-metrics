6fbed66f19a515c981a4030eaa099b2cf4eb9ddf
Sofi <Sofi.Rubizova@yandex.ru>
2023-09-07 16:20:40 +0300

    iteration 2 is completed

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,5 +1,94 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"runtime"
+	"strconv"
+	"time"
+)
 
+var valuesGauge = map[string]float64{}
+var pollCount uint64
+
+const pollInterval = 2
+const reportInterval = 10
+
+func getMetrics() {
+	var rtm runtime.MemStats
+	pollCount += 1
+	// Read full mem stats
+	runtime.ReadMemStats(&rtm)
+
+	valuesGauge["Alloc"] = float64(rtm.Alloc)
+	valuesGauge["BuckHashSys"] = float64(rtm.BuckHashSys)
+	valuesGauge["Frees"] = float64(rtm.Frees)
+	valuesGauge["GCCPUFraction"] = float64(rtm.GCCPUFraction)
+	valuesGauge["HeapAlloc"] = float64(rtm.HeapAlloc)
+	valuesGauge["HeapIdle"] = float64(rtm.HeapIdle)
+	valuesGauge["HeapInuse"] = float64(rtm.HeapInuse)
+	valuesGauge["HeapObjects"] = float64(rtm.HeapObjects)
+	valuesGauge["HeapReleased"] = float64(rtm.HeapReleased)
+	valuesGauge["HeapSys"] = float64(rtm.HeapSys)
+	valuesGauge["LastGC"] = float64(rtm.LastGC)
+	valuesGauge["Lookups"] = float64(rtm.Lookups)
+	valuesGauge["MCacheInuse"] = float64(rtm.MCacheInuse)
+	valuesGauge["MCacheSys"] = float64(rtm.MCacheSys)
+	valuesGauge["MSpanInuse"] = float64(rtm.MSpanInuse)
+	valuesGauge["MSpanSys"] = float64(rtm.MSpanSys)
+	valuesGauge["Mallocs"] = float64(rtm.Mallocs)
+	valuesGauge["NextGC"] = float64(rtm.NextGC)
+	valuesGauge["NumForcedGC"] = float64(rtm.NumForcedGC)
+	valuesGauge["NumGC"] = float64(rtm.NumGC)
+	valuesGauge["OtherSys"] = float64(rtm.OtherSys)
+	valuesGauge["PauseTotalNs"] = float64(rtm.PauseTotalNs)
+	valuesGauge["StackInuse"] = float64(rtm.StackInuse)
+	valuesGauge["StackSys"] = float64(rtm.StackSys)
+	valuesGauge["Sys"] = float64(rtm.Sys)
+	valuesGauge["TotalAlloc"] = float64(rtm.TotalAlloc)
+
+}
+
+func postQueries() {
+	for k, v := range valuesGauge {
+		post("gauge", k, strconv.FormatFloat(v, 'f', -1, 64))
+	}
+	post("counter", "PollCount", strconv.FormatUint(pollCount, 10))
+	post("gauge", "RandomValue", strconv.FormatFloat(rand.Float64(), 'f', -1, 64))
+	pollCount = 0
+}
+
+func post(t string, name string, value string) {
+	bodyReader := bytes.NewReader([]byte{})
+	url := "http://localhost:8080/update/" + t + "/" + name + "/" + value
+
+	// We can set the content type here
+	resp, err := http.Post(url, "text/plain", bodyReader)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Status:", resp.Status)
+}
+
+func main() {
+	pollTicker := time.NewTicker(time.Duration(pollInterval) * time.Second)
+	defer pollTicker.Stop()
+	reportTicker := time.NewTicker(time.Duration(reportInterval) * time.Second)
+	defer reportTicker.Stop()
+
+	for {
+		select {
+		case <-pollTicker.C:
+			getMetrics()
+			b, _ := json.Marshal(valuesGauge)
+			fmt.Println(string(b))
+		case <-reportTicker.C:
+			postQueries()
+		}
+	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,52 +1,14 @@
 package main
 
 import (
-	"github.com/Sofja96/go-metrics.git/internal/storage"
+	"github.com/Sofja96/go-metrics.git/internal/handlers"
 	"net/http"
-	"strconv"
-	"strings"
 )
 
-func metrics(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-	s := storage.New()
-	sliceURL := strings.Split(r.URL.Path, "/")
-
-	if len(sliceURL) != 5 || sliceURL[1] != "update" {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	metricsType := sliceURL[2]
-	metricsName := sliceURL[3]
-	metricsValue := sliceURL[4]
-	if metricsType == "counter" {
-		if value, err := strconv.ParseInt(metricsValue, 10, 64); err == nil {
-			s.UpdateCounter(metricsName, value)
-		} else {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-	} else if metricsType == "gauge" {
-		if value, err := strconv.ParseFloat(metricsValue, 64); err == nil {
-			s.UpdateGauge(metricsName, value)
-		} else {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-	} else {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-
-}
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, metrics)
+	mux.HandleFunc(`/`, handlers.Webhook)
+	//mux.HandleFunc(`/all`, handlers.AllMetrics)
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
 		panic(err)
