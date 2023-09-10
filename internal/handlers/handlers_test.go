dccbd58956e9347d9cc0da3feb7e305915bda14f
Sofi <Sofi.Rubizova@yandex.ru>
2023-09-10 18:16:03 +0300

    add test

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -131,6 +131,7 @@ func TestAllMetrics(t *testing.T) {
 				respBody, err := io.ReadAll(res.Body)
 				require.NoError(t, err)
 				assert.NotEmpty(len(respBody))
+				defer res.Body.Close()
 			}
 		})
 	}
